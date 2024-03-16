2d090f4d8e1fc0aca4a403ad543adc0187072ee0
BRIGHT <BRIGHT@DESKTOP-ST51CUU>
2024-03-16 10:19:43 +0300

    Add project files.

---
+++ Session_Kavramı/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Session_Kavramı.Models;
+using System.Diagnostics;
+using Microsoft.AspNetCore;
+
+namespace Session_Kavramı.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+     
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+
+            // Session tanımlaması : Session verilerimizi bir süre boyunca RAM'de saklayabildiğimiz bir ortamdır.
+            // Session'a değer attık. Session'a değer atarken hem değeri hemde attığımız değerin adını yazmamız gerekmektedir.
+            // Çünkü session bir sürü değer alabilir.Istenildiğinde  bu değeri size geri verebilmesi için attığınız değerin adını yazmanız lazım
+
+            // Redis in memory veri tutan bir üründür. Profesyonel ortamda session'lar için REdis kullanılır.
+
+
+            // Ödev : MVC'e redis'i entegre edip, session'ları redis'te tutmanız
+            HttpContext.Session.SetInt32("MyInt", 10);
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+           int deger =  (int)HttpContext.Session.GetInt32("MyInt");
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Session_Kavramı/Program.cs
@@ -0,0 +1,46 @@
+namespace Session_Kavramı
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            //Session MiddleWare(Session'in ne kadar süre ile RAM'de tutulacağı bilgisini buradan ayarladık
+            builder.Services.AddSession(option => {
+
+
+                option.IdleTimeout = TimeSpan.FromMinutes(1);
+            
+            
+            });
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            // Session kullanacağımz session'un tanımlamasını yaptık
+            app.UseSession();
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
